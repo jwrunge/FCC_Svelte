ec0a67e68ebeeb6c6e7c4d34e97234c56bbcb8fc
jwr <jwrunge@gmail.com>
2025-10-01 00:12:12 -0500

    Forward /php to local dev server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+	plugins: [svelte()],
+	server: {
+		proxy: {
+			"/data/php": {
+				target: "http://localhost:9000",
+				changeOrigin: true,
+				// leave path as-is so /data/php/... hits the PHP server
+			},
+		},
+	},
+});
